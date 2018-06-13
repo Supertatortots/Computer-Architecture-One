199d1b9fb88f3f28d6481bbe79e673e88cc6c8c7
totsuya <supertatortots@gmail.com>
2018-06-13 16:35:55 -0400

    unhardcoded program so now it reads print8 (mult still wip)

--- project/ls8/ls8.js
+++ project/ls8/ls8.js
@@ -1,25 +1,36 @@
 const RAM = require('./ram');
 const CPU = require('./cpu');
 const fs = require('fs');
-const items = [];
 /**
  * Load an LS8 program into memory
  *
  * TODO: load this from a file on disk instead of having it hardcoded
  */
 function loadMemory() {
+    const argv = process.argv.slice(2);
+
+    if (argv.length != 1) {
+        console.error("usage: program filename");
+        process.exit(1);
+    }
+
+    const filename = argv[0];
+    const fileData = fs.readFileSync(filename, "utf8").trim().split(/[\r\n]+/g).filter(line => line[0] != '#');
+    const program = [];
+    fileData.forEach(data => program.push(data));
 
     // Hardcoded program to print the number 8 on the console
 
-    // const program = [ // print8.ls8
-    //     "10011001", // LDI R0,8  Store 8 into R0
-    //     "00000000",
-    //     "00001000",
-    //     "01000011", // PRN R0    Print the value in R0
-    //     "00000000",
-    //     "00000001"  // HLT       Halt and quit
-    // ];
-    // const program = [
+        // const program = [ // print8.ls8
+        //     "10011001", // LDI R0,8  Store 8 into R0
+        //     "00000000",
+        //     "00001000",
+        //     "01000011", // PRN R0    Print the value in R0
+        //     "00000000",
+        //     "00000001"  // HLT       Halt and quit
+        // ];
+
+    // const program = [ //mult
     //     "10011001", // LDI R0, 8
     //     "00000000",
     //     "00001000",
@@ -34,9 +45,11 @@ function loadMemory() {
     //     "00000001" // HLT
     // ];
     // Load the program into the CPU's memory a byte at a time
+
     for (let i = 0; i < program.length; i++) {
         cpu.poke(i, parseInt(program[i], 2));
     }
+
 }
 
 /**
@@ -48,20 +61,7 @@ let cpu = new CPU(ram);
 
 // TODO: get name of ls8 file to load from command line
 
-const argv = process.argv.slice(2);
 
-if (argv.length != 1) {
-    console.error("Requires file to read!");
-    process.exit(1);
-}
-
-const filename = argv[0];
-const filedata = fs.readFileSync(filename, "utf8");
-const lines = filedata.trim().split(/[\r\n]+/g);
-
-lines.forEach((bin, idx) => {
-    if (idx) cpu.poke(idx, parseInt(bin.split(' ')[0], 2));
-});
 
 loadMemory(cpu);
 
