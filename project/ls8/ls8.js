cf0ad6493ca5079ce069c222c3bd7db2c8998633
totsuya <supertatortots@gmail.com>
2018-06-13 15:54:39 -0400

    commented out hardcoded program

--- project/ls8/ls8.js
+++ project/ls8/ls8.js
@@ -10,14 +10,14 @@ function loadMemory() {
 
     // Hardcoded program to print the number 8 on the console
 
-    const program = [ // print8.ls8
-        "10011001", // LDI R0,8  Store 8 into R0
-        "00000000",
-        "00001000",
-        "01000011", // PRN R0    Print the value in R0
-        "00000000",
-        "00000001"  // HLT       Halt and quit
-    ];
+    // const program = [ // print8.ls8
+    //     "10011001", // LDI R0,8  Store 8 into R0
+    //     "00000000",
+    //     "00001000",
+    //     "01000011", // PRN R0    Print the value in R0
+    //     "00000000",
+    //     "00000001"  // HLT       Halt and quit
+    // ];
 
     // Load the program into the CPU's memory a byte at a time
     for (let i = 0; i < program.length; i++) {
@@ -34,20 +34,20 @@ let cpu = new CPU(ram);
 
 // TODO: get name of ls8 file to load from command line
 
-// const argv = process.argv.slice(2);
+const argv = process.argv.slice(2);
 
-// if (argv.length != 1) {
-//     console.error("Requires file to read!");
-//     process.exit(1);
-// }
+if (argv.length != 1) {
+    console.error("Requires file to read!");
+    process.exit(1);
+}
 
-// const filename = argv[0];
-// const filedata = fs.readFileSync(filename, "utf8");
-// const lines = filedata.trim().split(/[\r\n]+/g);
+const filename = argv[0];
+const filedata = fs.readFileSync(filename, "utf8");
+const lines = filedata.trim().split(/[\r\n]+/g);
 
-// lines.forEach((bin, idx) => {
-//     if (idx) cpu.poke(idx, parseInt(bin.split(' ')[0], 2));
-// });
+lines.forEach((bin, idx) => {
+    if (idx) cpu.poke(idx, parseInt(bin.split(' ')[0], 2));
+});
 
 loadMemory(cpu);
 
